5adc27caf30899a96b3551b13778b751332c6c1d
Brock Chelle <Brocklchelle@gmail.com>
2021-02-01 07:48:10 -0700

    Changed the login resolver to a query instead of a mutation.
    No data is getting changed when logging in.

--- server/src/resolvers/user.resolver.ts
+++ server/src/resolvers/user.resolver.ts
@@ -20,6 +20,9 @@ import {
 	validateUserDoesNotExist,
 } from '../validators/user.validators';
 
+/**
+ * Input structure for methods that require a username and password
+ */
 @InputType()
 class UsernamePasswordInput implements Partial<User> {
 	@Field()
@@ -29,8 +32,11 @@ class UsernamePasswordInput implements Partial<User> {
 	password: string;
 }
 
+/**
+ * Response structure for user queries and mutations
+ */
 @ObjectType()
-export class UserResponse {
+class UserResponse {
 	@Field(() => [FieldError], { nullable: true })
 	errors?: FieldError[];
 
@@ -48,6 +54,48 @@ export class UserResolver {
 	@Query(() => User)
 	async user(@Arg('id') id: number, @Ctx() { prisma }: Context) {
 		return await prisma.user.findUnique({ where: { id } });
+	}
+
+	/**
+	 * Attempts to log a user in with given credentials
+	 * @param options The username and password credentials
+	 * @param prisma The prisma client
+	 */
+	@Query(() => UserResponse)
+	async login(
+		@Arg('options') options: UsernamePasswordInput,
+		@Ctx() { prisma }: Context,
+	): Promise<UserResponse> {
+		// Pulls the username and password off of the options
+		const { username, password } = options;
+
+		// Attempts to find a user with the passed in username
+		const user = await prisma.user.findUnique({
+			where: { username },
+		});
+
+		// Sends an error if the username wasn't found
+		if (!user) {
+			return {
+				errors: [
+					{
+						field: 'username',
+						message: `Could not find a user with the username ${username}`,
+					},
+				],
+			};
+		}
+
+		// Checks that the password matches
+		const errors = await validatePasswordMatch(user.password, password);
+
+		if (errors.length > 0) {
+			return { errors };
+		}
+
+		// If the code reaches here, the user exists and the password matches
+		// Sends the user their information
+		return { user };
 	}
 
 	/**
@@ -82,48 +130,6 @@ export class UserResolver {
 			data: { username, password: hashedPassword },
 		});
 
-		return { user };
-	}
-
-	/**
-	 * Attempts to log a user in with given credentials
-	 * @param options The username and password credentials
-	 * @param prisma The prisma client
-	 */
-	@Mutation(() => UserResponse)
-	async login(
-		@Arg('options') options: UsernamePasswordInput,
-		@Ctx() { prisma }: Context,
-	): Promise<UserResponse> {
-		// Pulls the username and password off of the options
-		const { username, password } = options;
-
-		// Attempts to find a user with the passed in username
-		const user = await prisma.user.findUnique({
-			where: { username },
-		});
-
-		// Sends an error if the username wasn't found
-		if (!user) {
-			return {
-				errors: [
-					{
-						field: 'username',
-						message: `Could not find a user with the username ${username}`,
-					},
-				],
-			};
-		}
-
-		// Checks that the password matches
-		const errors = await validatePasswordMatch(user.password, password);
-
-		if (errors.length > 0) {
-			return { errors };
-		}
-
-		// If the code reaches here, the user exists and the password matches
-		// Sends the user their information
 		return { user };
 	}
 }
