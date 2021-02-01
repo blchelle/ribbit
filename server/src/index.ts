5fea74bb0f8109a3be3ee093bdee815f93941889
Brock Chelle <Brocklchelle@gmail.com>
2021-02-01 09:03:42 -0700

    Implemented the setup for redis middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,17 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import session from 'express-session';
 import morgan from 'morgan';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
 import { PrismaClient } from '@prisma/client';
 
 import { PostResolver } from './resolvers/post.resolver';
 import { UserResolver } from './resolvers/user.resolver';
-import { __PORT__, __PROD__ } from './constants';
+import { __PORT__, __PROD__, __REDIS_SECRET__ } from './constants';
 
 export interface Context {
 	prisma: PrismaClient;
@@ -19,12 +22,31 @@ const prisma = new PrismaClient({
 	log: __PROD__ ? [] : ['query', 'error', 'info', 'warn'],
 });
 
-let main = async () => {
+const main = async () => {
 	try {
 		const app = express();
 
 		if (!__PROD__) app.use(morgan('dev'));
 
+		// Throws if the redis secret isn't set
+		if (!__REDIS_SECRET__) {
+			throw new Error('Redis Secret Environment Variable is Undefined');
+		}
+
+		const RedisStore = connectRedis(session);
+		const redisClient = redis.createClient();
+
+		// Applies redis store - session middleware
+		app.use(
+			session({
+				name: 'qid',
+				store: new RedisStore({ client: redisClient }),
+				secret: __REDIS_SECRET__,
+				resave: false,
+			}),
+		);
+
+		// Sets up apollo server middleware
 		const apolloServer = new ApolloServer({
 			schema: await buildSchema({
 				resolvers: [UserResolver, PostResolver],
@@ -34,8 +56,10 @@ let main = async () => {
 			playground: !__PROD__,
 		});
 
+		// Applies the apollos server middleware
 		apolloServer.applyMiddleware({ app });
 
+		// Start the server
 		app.listen(__PORT__, () =>
 			console.log(`GraphQL API on port ${__PORT__}`),
 		);
