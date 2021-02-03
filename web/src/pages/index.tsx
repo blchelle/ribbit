8a520edbd9cd2e652cbf34983d45a4292a3eacbc
Brock Chelle <Brocklchelle@gmail.com>
2021-02-03 08:58:48 -0700

    Basic styling for the landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,15 @@
-const Index = () => <div>Hello World</div>;
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+import NavBar from '../components/NavBar';
+
+const Index = () => (
+	<>
+		<NavBar>
+			<div>Hello World</div>;
+		</NavBar>
+		<Heading>Ribbit</Heading>
+	</>
+);
 
 export default Index;
