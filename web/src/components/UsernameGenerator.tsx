07a9de5023a2dc2dac1dfe65b730a197e57a7d6e
Brock Chelle <Brocklchelle@gmail.com>
2021-02-06 09:14:34 -0700

    Created a UsernameGenerator component
    This takes in a set of usernames and displays them so that the user doesn't have to come up with their own username

---
+++ web/src/components/UsernameGenerator.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useField } from 'formik';
+import {
+	Button,
+	Flex,
+	IconButton,
+	Stack,
+	StackProps,
+	Text,
+} from '@chakra-ui/react';
+import { IoReload } from 'react-icons/io5';
+
+interface UsernameGeneratorProps extends StackProps {
+	name: string;
+	usernames: string[];
+	reload: () => void;
+}
+const UsernameGenerator: React.FC<UsernameGeneratorProps> = ({
+	name,
+	usernames,
+	reload,
+	...props
+}) => {
+	// Formik Hook for setting the username field
+	const [, , helpers] = useField(name);
+
+	return (
+		<Stack align="flex-start" spacing={0} {...props}>
+			<Flex alignItems="center">
+				<Text mr={2}>Here are some username suggestions</Text>
+				<IconButton
+					aria-label="reload-usernames"
+					onClick={reload}
+					variant="ghost"
+					colorScheme="secondary"
+				>
+					<IoReload />
+				</IconButton>
+			</Flex>
+			{usernames.map((username) => (
+				<>
+					<Button
+						colorScheme="secondary"
+						height={6}
+						variant="link"
+						onClick={() => helpers.setValue(username)}
+					>
+						{username}
+					</Button>
+				</>
+			))}
+		</Stack>
+	);
+};
+
+export default UsernameGenerator;
