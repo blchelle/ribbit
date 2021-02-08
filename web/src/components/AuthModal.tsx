860ba945d6dff458b607f8c3c441b384817281d5
Brock Chelle <Brocklchelle@gmail.com>
2021-02-08 08:54:23 -0700

    Added image to AuthModal
    The current image is pulled from reddit.

--- web/src/components/AuthModal.tsx
+++ web/src/components/AuthModal.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Image from 'next/image';
 import {
+	Flex,
 	Modal,
 	ModalOverlay,
 	ModalContent,
 	ModalProps,
+	ModalCloseButton,
+	Box,
 } from '@chakra-ui/react';
 import Login from './Login';
 import Register from './Register';
@@ -12,17 +16,31 @@ export type AuthModalType = 'login' | 'sign up' | 'forgot password';
 
 interface AuthModalProps extends ModalProps {
 	type: AuthModalType;
+	setType: React.Dispatch<React.SetStateAction<AuthModalType>>;
 }
 
-const AuthModal: React.FC<AuthModalProps> = ({ type, ...props }) => {
+const AuthModal: React.FC<AuthModalProps> = ({ type, setType, ...props }) => {
 	let form = null;
-	if (type === 'login') form = <Login onClose={props.onClose} />;
-	else if (type === 'sign up') form = <Register onClose={props.onClose} />;
+	if (type === 'login')
+		form = <Login onClose={props.onClose} setType={setType} />;
+	else if (type === 'sign up')
+		form = <Register onClose={props.onClose} setType={setType} />;
 
 	return (
-		<Modal {...props}>
+		<Modal {...props} size="3xl">
 			<ModalOverlay />
-			<ModalContent p={4}>{form}</ModalContent>
+			<ModalContent h={500}>
+				<ModalCloseButton />
+				<Flex h="100%">
+					<Image
+						src="/auth-left.png"
+						width="130px"
+						height="100%"
+						objectFit="cover"
+					/>
+					<Box p={8}>{form}</Box>
+				</Flex>
+			</ModalContent>
 		</Modal>
 	);
 };
